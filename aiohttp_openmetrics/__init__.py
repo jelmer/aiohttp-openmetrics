1ab48f9898800655403099e0ce8594e5f605faed
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-01-02 10:20:20 +0000

    Implement push_to_gateway.

--- aiohttp_openmetrics/__init__.py
+++ aiohttp_openmetrics/__init__.py
@@ -11,9 +11,14 @@ __all__ = [
     'run_prometheus_server',
     ]
 
-from aiohttp import web
 import asyncio
+import base64
 import time
+from urllib.parse import quote_plus
+
+from aiohttp import web
+from aiohttp.client import ClientSession, ClientTimeout
+from yarl import URL
 
 from prometheus_client import (
     Counter,
@@ -82,3 +87,29 @@ async def run_prometheus_server(listen_addr, port):
     await runner.setup()
     site = web.TCPSite(runner, listen_addr, port)
     await site.start()
+
+
+# _escape_grouping_key imported from pprometheus-client @ https://github.com/prometheus/client_python
+def _escape_grouping_key(k, v):
+    if v == "":
+        # Per https://github.com/prometheus/pushgateway/pull/346.
+        return k + "@base64", "="
+    elif '/' in v:
+        # Added in Pushgateway 0.9.0.
+        return k + "@base64", base64.urlsafe_b64encode(v.encode("utf-8")).decode("utf-8")
+    else:
+        return k, quote_plus(v)
+
+
+async def push_to_gateway(gateway, job, registry, timeout=30):
+    (k, v) = _escape_grouping_key("job", job)
+    url = URL(gateway) / "metrics" / k / v
+
+    data = generate_latest(registry)
+
+    async with ClientSession() as session:
+        async with session.put(
+                url, timeout=ClientTimeout(timeout),
+                content_type=CONTENT_TYPE_LATEST, data=data,
+                raise_for_status=True):
+            pass
