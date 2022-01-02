6fb3e41491f3ac4975de2fa615236f931234c46c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-01-02 11:16:02 +0000

    Fix formatting.

--- aiohttp_openmetrics/__init__.py
+++ aiohttp_openmetrics/__init__.py
@@ -89,14 +89,17 @@ async def run_prometheus_server(listen_addr, port):
     await site.start()
 
 
-# _escape_grouping_key imported from pprometheus-client @ https://github.com/prometheus/client_python
+# _escape_grouping_key imported from pprometheus-client @
+# https://github.com/prometheus/client_python
 def _escape_grouping_key(k, v):
     if v == "":
         # Per https://github.com/prometheus/pushgateway/pull/346.
         return k + "@base64", "="
     elif '/' in v:
         # Added in Pushgateway 0.9.0.
-        return k + "@base64", base64.urlsafe_b64encode(v.encode("utf-8")).decode("utf-8")
+        return (
+            k + "@base64",
+            base64.urlsafe_b64encode(v.encode("utf-8")).decode("utf-8"))
     else:
         return k, quote_plus(v)
 
